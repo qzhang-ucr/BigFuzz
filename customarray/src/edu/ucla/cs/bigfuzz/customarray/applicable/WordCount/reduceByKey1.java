0dcd269b87effbf544e2483008644018179f34a8
Jiyuan Wang <wangjiyuan123@yeah.net>
2020-12-02 16:35:22 -0800

    check Movierating/wordcount

--- customarray/src/edu/ucla/cs/bigfuzz/customarray/applicable/WordCount/reduceByKey1.java
+++
@@ -1,7 +0,0 @@
-package edu.ucla.cs.bigfuzz.customarray.applicable.WordCount;
-
-public class reduceByKey1 {
-            public static Integer apply(Integer first, Integer second) throws Exception {
-                return first + second;
-            }
-        }
